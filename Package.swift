fd3a6bc2f1c860bb641be915ff1eb74f8fab4cb8
Stefan Trauth <mail@stefantrauth.de>
2017-11-02 21:21:12 +0100

    codable alamofire already requires alamofire

--- Package.swift
+++ Package.swift
@@ -14,15 +14,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.5.1"),
-        .package(url: "https://github.com/Otbivnoe/CodableAlamofire.git", from: "1.1.0"),
+        .package(url: "https://github.com/Otbivnoe/CodableAlamofire.git", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FyydKit",
-            dependencies: ["Alamofire", "CodableAlamofire"]),
+            dependencies: ["CodableAlamofire"],
         .testTarget(
             name: "FyydKitTests",
             dependencies: ["FyydKit"]
