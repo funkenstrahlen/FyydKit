a195ac3ff6413b235a98119fa3ec418d53f5db62
Stefan Trauth <mail@stefantrauth.de>
2017-11-02 21:21:00 +0100

    refactor

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "FyydKit",
-            targets: ["FyydKit"]),
+            targets: ["FyydKit"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,6 +25,7 @@ let package = Package(
             dependencies: ["Alamofire", "CodableAlamofire"]),
         .testTarget(
             name: "FyydKitTests",
-            dependencies: ["FyydKit"]),
+            dependencies: ["FyydKit"]
+        )
     ]
 )
